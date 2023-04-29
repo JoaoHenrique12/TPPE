ca1bba82fbe5705436671c84a0451457e49592f2
Joao Henrique <joao.henrique1299@hotmail.com>
2023-04-29 06:57:22 -0300

    Adding fixures into tests

--- init/tdd_one/test_format_data.py
+++ init/tdd_one/test_format_data.py
@@ -1,6 +1,9 @@
+import pytest
+
 from format_data import *
 
-def test_format_data_for_display():
+@pytest.fixture
+def example_people_data():
     people = [
         {
             "given_name": "Alfonsa",
@@ -13,27 +16,18 @@ def test_format_data_for_display():
             "title": "Project Manager",
         },
     ]
+    return people
 
-    assert format_data_for_display(people) == [
+def test_format_data_for_display(example_people_data):
+
+    assert format_data_for_display(example_people_data) == [
         "Alfonsa Ruiz: Senior Software Engineer",
         "Sayid Khan: Project Manager",
     ]
 
-def test_format_data_for_excel():
-    people = [
-        {
-            "given_name": "Alfonsa",
-            "family_name": "Ruiz",
-            "title": "Senior Software Engineer",
-        },
-        {
-            "given_name": "Sayid",
-            "family_name": "Khan",
-            "title": "Project Manager",
-        },
-    ]
+def test_format_data_for_excel(example_people_data):
 
-    assert format_data_for_excel(people) == """given,family,title
+    assert format_data_for_excel(example_people_data) == """given,family,title
 Alfonsa,Ruiz,Senior Software Engineer
 Sayid,Khan,Project Manager
 """
