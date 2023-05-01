b1a0e2bc6fcca162c1a6ad499bf53e12a01928b5
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-01 18:59:18 -0300

    (LCS) Adding Triangulation to tests

--- LCS/test_lcs.py
+++ LCS/test_lcs.py
@@ -3,7 +3,11 @@ import pytest
 from lcs import lcs
 
 @pytest.mark.parametrize("st1, st2, size_lcs",[ 
-    ("abc", "abc", 3)
+    ("", "", 0),
+    ("abc", "abc", 3),
+    ("racecarer", "racecarer", 9),
+    ("long", "tooth in goal", 3),
+    ("abc123", "ab23ac23abc23", 5),
  ])
 def test_lcs(st1, st2, size_lcs):
     assert lcs(st1, st2) == size_lcs
