0814e87f3611fe7abfe7c19e3e632a17bb365f78
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-01 19:23:44 -0300

    (LCS) Refactoring code to achieve green bar.

--- LCS/lcs.py
+++ LCS/lcs.py
@@ -1,2 +1,20 @@
 def lcs(st1, st2):
-    return min(len(st1), len(st2))
+    smaller_string = min(st1, st2)
+    bigger_string = max(st1, st2)
+
+    if len(smaller_string) == 0:
+        return 0
+
+    count_lcs = [0] * len(smaller_string)
+
+    for i in range(len(smaller_string)):
+        runner = i
+        for j in range(len(bigger_string)):
+            if smaller_string[runner] == bigger_string[j]:
+                count_lcs[i] += 1
+                runner += 1
+
+            if runner >= len(smaller_string):
+                break
+
+    return max(count_lcs)
--- LCS/test_lcs.py
+++ LCS/test_lcs.py
@@ -8,6 +8,7 @@ from lcs import lcs
     ("racecarer", "racecarer", 9),
     ("long", "tooth in goal", 3),
     ("abc123", "ab23ac23abc23", 5),
+    ("12345","23415",4)
  ])
 def test_lcs(st1, st2, size_lcs):
     assert lcs(st1, st2) == size_lcs
